399428134c8a1b90b3b1ac2ffdfc81eec15733e5
Qaz Cetelic <51381523+QazCetelic@users.noreply.github.com>
2021-12-15 21:45:39 +0100

    Refactoring for clarity

--- src/main/kotlin/Filters.kt
+++ src/main/kotlin/Filters.kt
@@ -1,30 +1,46 @@
 import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
 import java.nio.charset.Charset
+import java.util.*
 import javax.swing.filechooser.FileFilter
 
 val svgFilter = object: FileFilter() {
-    override fun accept(f: File): Boolean = f.isDirectory || f.extension.equals("SVG", ignoreCase = true)
-    override fun getDescription(): String = "SVG"
+    override fun accept(file: File): Boolean = file.isDirectory || file.extension.equals("SVG", ignoreCase = true)
+    override fun getDescription() = "SVG"
 }
 
 val watermarkedSvgFilter = object: FileFilter() {
-    override fun accept(f: File): Boolean {
-        if (f.isDirectory) return true
-        if (!f.extension.equals("SVG", ignoreCase = true)) return false
+    override fun accept(file: File): Boolean {
+        if (file.isDirectory) return true
+        if (!file.extension.equals("SVG", ignoreCase = true)) return false
 
-        // Reads a certain region of bytes in a file and checks if it matches the regex
+        val stream = FileInputStream(file)
+        if (128 > stream.available()) return false
+        stream.skip(128)
+        val charBuffer2 = buildString {
+            for (i in 0..(minOf(256, stream.available()))) {
+                val b = stream.read().toByte()
+                append(b.toChar())
+            }
+        }
+        println(charBuffer2)
+
+        // Checks if it matches the regex if it's uncertain if it's watermarked
+        // Reads a certain region of bytes in a file
         val skippedBytes = 128
-        val readBytes = 256
-        val totalBytes = skippedBytes + readBytes
-        if (f.length() < totalBytes) return false
-        val bReader = f.bufferedReader(Charset.defaultCharset(), totalBytes)
+        val readBytes    = 256
+        val totalBytes   = skippedBytes + readBytes
+        if (totalBytes > file.length()) return false
+
+        val bReader = file.bufferedReader(Charset.defaultCharset(), totalBytes)
         bReader.skip(skippedBytes.toLong())
         val charBuffer = buildString {
-            for (i in 0..readBytes) {
+            repeat(readBytes) {
                 append(bReader.read().toChar())
             }
         }
         return unregisteredTextRegex in charBuffer
     }
-    override fun getDescription(): String = "Watermarked SVG"
+    override fun getDescription() = "Watermarked SVG"
 }
--- src/main/kotlin/RemoveWatermark.kt
+++ src/main/kotlin/RemoveWatermark.kt
@@ -1,11 +1,13 @@
 import java.io.File
 
 val unregisteredTextRegex = Regex("<text([^>])+>UNREGISTERED</text>")
+
 fun removeWatermarkFromSVG(svgString: String): String {
     return svgString.replace(unregisteredTextRegex, "")
 }
 
 fun File.removeWatermark() {
     val svgString = readText()
-    writeText(removeWatermarkFromSVG(svgString))
+    val svgStringWithoutWatermark = removeWatermarkFromSVG(svgString)
+    writeText(svgStringWithoutWatermark)
 }
