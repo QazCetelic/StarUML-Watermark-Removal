9a29dc10907563b6e6e105077c686b21a968f441
A2va <49582555+A2va@users.noreply.github.com>
2023-09-28 21:41:07 +0200

    Update Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.9.10"
     id("edu.sc.seis.launch4j") version "2.5.0"
 }
 
