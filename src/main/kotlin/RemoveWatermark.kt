7589485baca130e3f9edf8fd28a4bf1e0c36377b
A2va <49582555+A2va@users.noreply.github.com>
2023-09-29 10:01:46 +0200

    Add pdf support

--- src/main/kotlin/RemoveWatermark.kt
+++ src/main/kotlin/RemoveWatermark.kt
@@ -5,6 +5,15 @@ import org.apache.batik.transcoder.TranscoderOutput
 import org.apache.batik.transcoder.image.PNGTranscoder
 import org.apache.batik.transcoder.svg2svg.SVGTranscoder
 import org.apache.batik.util.XMLResourceDescriptor
+import org.apache.pdfbox.contentstream.operator.Operator
+import org.apache.pdfbox.cos.COSArray
+import org.apache.pdfbox.cos.COSName
+import org.apache.pdfbox.cos.COSString
+import org.apache.pdfbox.pdfparser.PDFStreamParser
+import org.apache.pdfbox.pdfwriter.ContentStreamWriter
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.PDPage
+import org.apache.pdfbox.pdmodel.common.PDStream
 import org.w3c.dom.Document
 import org.w3c.dom.NodeList
 import java.io.File
@@ -12,22 +21,98 @@ import java.io.FileOutputStream
 import java.io.IOException
 import java.io.OutputStreamWriter
 import java.nio.charset.StandardCharsets
+import java.nio.file.Files
 
 val unregisteredTextRegex = Regex("<text([^>])+>UNREGISTERED</text>")
 
 fun File.withoutWatermark(convertToImage: Boolean) {
-    // TODO use less hacky method to remove watermark
-    stupidWatermarkRemoval()
-    val svg = toSVGDocument().withoutWatermark()
-    writeSvg(svg)
-    if (convertToImage) {
-        val bitmapFile = File(parentFile, "$nameWithoutExtension.png")
-        bitmapFile.writeSvgAsBitmap(svg)
+
+    when (Files.probeContentType(toPath())) {
+        "image/svg+xml" -> {
+            // TODO use less hacky method to remove watermark
+            stupidWatermarkRemoval()
+
+            val svg = toSVGDocument().withoutWatermark()
+            writeSvg(svg)
+            if (convertToImage) {
+                val bitmapFile = File(parentFile, "$nameWithoutExtension.png")
+                bitmapFile.writeSvgAsBitmap(svg)
+            }
+        }
+        "application/pdf" -> {
+            val pdf = toPDFDocument().withoutWatermark()
+            writePdf(pdf)
+        }
     }
+
 }
 
 fun File.stupidWatermarkRemoval() {
     writeText(readText().replace(unregisteredTextRegex, ""))
+}
+
+fun PDDocument.withoutWatermark(): PDDocument {
+    val searchString = "UNREGISTERED"
+    val replacement = ""
+
+    for (page: PDPage in getPages()){
+        val parser = PDFStreamParser(page)
+        parser.parse()
+        val tokens = parser.tokens
+
+        for (j in 0 until tokens.size) {
+            val next = tokens[j]
+            if(next !is Operator) {
+                continue
+            }
+
+            val op = next
+
+            var pstring = ""
+            var prej = 0
+
+            //Tj and TJ are the two operators that display strings in a PDF
+            if (op.name == "Tj") {
+                // Tj takes one operator and that is the string to display so lets update that operator
+                val previous = tokens[j - 1] as COSString
+                var string = previous.getString()
+                string = string.replaceFirst(searchString.toRegex(), replacement)
+                previous.setValue(string.toByteArray())
+            } else if (op.name == "TJ") {
+                val previous = tokens[j - 1] as COSArray
+                for (k in 0 until previous.size()) {
+                    val arrElement: Any = previous.getObject(k)
+                    if (arrElement is COSString) {
+                        val string = arrElement.getString()
+                        if (j === prej) {
+                            pstring += string
+                        } else {
+                            prej = j
+                            pstring = string
+                        }
+                    }
+                }
+                if (searchString.equals(pstring.trim { it <= ' ' })) {
+                    val cosString2 = previous.getObject(0) as COSString
+                    cosString2.setValue(replacement.toByteArray())
+                    val total = previous.size() - 1
+                    for (k in total downTo 1) {
+                        previous.remove(k)
+                    }
+                }
+            }
+
+        }
+        // Now that the tokens are updated, replace the page content stream.
+        val updatedStream = PDStream(document)
+        val out = updatedStream.createOutputStream(COSName.FLATE_DECODE)
+        val tokenWriter = ContentStreamWriter(out)
+        tokenWriter.writeTokens(tokens)
+        out.close()
+        page.setContents(updatedStream)
+    }
+
+    return this
 }
 
 fun Document.withoutWatermark(): Document {
@@ -53,6 +138,10 @@ fun File.toSVGDocument(): Document {
     return SAXSVGDocumentFactory(XMLResourceDescriptor.getXMLParserClassName()).createSVGDocument(toURI().toString())
 }
 
+fun File.toPDFDocument(): PDDocument {
+    return PDDocument.load(this)
+}
+
 fun File.writeSvg(svg: Document) {
     val outputSvg = File(path)
     if (!outputSvg.exists()) outputSvg.createNewFile()
@@ -61,6 +150,11 @@ fun File.writeSvg(svg: Document) {
         val output = TranscoderOutput(writer)
         SVGTranscoder().transcode(TranscoderInput(svg), output)
     }
+}
+
+fun File.writePdf(pdf: PDDocument) {
+    val outputPdf = File(path)
+    pdf.save(outputPdf)
 }
 
 fun File.writeSvgAsBitmap(svg: Document) {
